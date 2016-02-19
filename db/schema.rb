5c39eac68046b348fc6f34335f908f0cc09c1e9a
Roman Drobjazko <roman_drobjatzko@hotmail.de>
2016-02-20 00:21:16 +0100

    push zum server

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218143202) do
+ActiveRecord::Schema.define(version: 20160219211738) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
