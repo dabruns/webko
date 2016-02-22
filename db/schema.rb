6698bc6ecde784edb3745265807e8f2cac5a3796
Roman Drobjazko <roman_drobjatzko@hotmail.de>
2016-02-22 13:00:35 +0100

    erste anpassungen an aktiv admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221105217) do
+ActiveRecord::Schema.define(version: 20160221105528) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -102,6 +102,16 @@ ActiveRecord::Schema.define(version: 20160221105217) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "user_groups", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_groups", ["group_id"], name: "index_user_groups_on_group_id"
+  add_index "user_groups", ["user_id"], name: "index_user_groups_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",               default: "", null: false
