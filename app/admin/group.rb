0fab0e09b7816b40b2017eb9e7d46d359e78f296
Roman Drobjazko <roman_drobjatzko@hotmail.de>
2016-02-23 12:49:39 +0100

    anderungen in aktive admin

--- app/admin/group.rb
+++ app/admin/group.rb
@@ -4,6 +4,7 @@ ActiveAdmin.register Group do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
+  permit_params :title, :deskription
 #
 # or
 #
