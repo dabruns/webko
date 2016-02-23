e60cafbb686723f25fdc37632c4e02ec296c191d
Roman Drobjazko <roman_drobjatzko@hotmail.de>
2016-02-23 20:09:51 +0100

    bereit zum push

---
+++ app/admin/user_group.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register UserGroup do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+ permit_params :group_id, :user_id
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+  index do
+    column 'Gruppe', :group_id
+    column 'Benutzer', :user_id
+    actions
+  end
+
+
+end
