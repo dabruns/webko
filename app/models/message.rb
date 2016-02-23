b9bf5a8d23c2fe40a6ffef8620162a864659cb95
Sebi <sroelfes1@gmail.com>
2016-02-23 15:56:54 +0100

    Nochmals Design angepasst, kleinere Fehler behoben

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,6 @@
 class Message < ActiveRecord::Base
       validates :title, presence: true
       validates :description, presence: true
-      validates :date, presence: true
+      validates :date, presence: false
       validates :important, presence: false
-
-
 end
