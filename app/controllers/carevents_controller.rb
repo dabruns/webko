1725cb7fb899696cfac659c302ab16d85fab9512
Roman Drobjazko <roman_drobjatzko@hotmail.de>
2016-02-24 11:49:25 +0100

    weitere rubocop fehler gefixt

--- app/controllers/carevents_controller.rb
+++ app/controllers/carevents_controller.rb
@@ -80,7 +80,8 @@ class CareventsController < ApplicationController
     @carevent = Carevent.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
+  # Never trust parameters from the scary internet,
+  # only allow the white list through.
   def carevent_params
     params.require(:carevent).permit(:title, :description, :start_time, :end_time, :car_id)
   end
