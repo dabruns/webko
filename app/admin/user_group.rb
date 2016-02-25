fd3a03ad5c0f01debdb74cf60d345d572278cee7
Roman Drobjazko <roman_drobjatzko@hotmail.de>
2016-02-25 17:03:56 +0100

    bereit zum merge

--- app/admin/user_group.rb
+++ app/admin/user_group.rb
@@ -15,16 +15,13 @@ ActiveAdmin.register UserGroup do
   # end
 
   index do
-    column 'Gruppe', :group_id, sortable: :group_id do |group|
-      div do
-        Group.find(group).title
-      end
-    end
-    column 'Benutzer', :user_id, sortable: :user_id do |user|
-      div class: 'user' do
-        User.find(user).username
-      end
-    end
+    #column 'Gruppe', :group_id, sortable: :group_id do |group|
+    #  div do
+    #    Group.find(group).title
+    #  end
+    #end
+    column 'Gruppe', :group_id
+    column 'Benutzer', :user_id
     actions
   end
 end
