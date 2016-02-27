f6133b882cfa1135a3d1e805764b5928a3cad75a
Roman Drobjazko <roman_drobjatzko@hotmail.de>
2016-02-27 16:46:34 +0100

    bugfix

--- app/models/carevent.rb
+++ app/models/carevent.rb
@@ -4,5 +4,5 @@ class Carevent < ActiveRecord::Base
   belongs_to :user
   validates :title, :description, presence:  true
   validates_datetime :start_time, before: :end_time, after: :today
-  validates_datetime :end_time, after.call (:start_time, :today)
+  validates_datetime :end_time, after: :start_time, after: :today
 end
