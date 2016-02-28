500909f0107f4e1e7bca2e850a6ce946fa2275b9
Roman Drobjazko <roman_drobjatzko@hotmail.de>
2016-02-28 23:10:34 +0100

    rspec alle fehler behoben :)

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   before :each do
-    comment = Comment.new(text: 'ein kommentar')
+    Comment.new(text: 'ein kommentar')
   end
 
   describe '#new' do
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -10,11 +10,11 @@ module ControllerMacros
     before(:each) do
       @request.env['devise.mapping'] = Devise.mappings[:user]
       user = FactoryGirl.create(:user)
-      user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
+      user.confirm! # or set a confirmed_at inside the factory.
+      # Only necessary if you are using the "confirmable" module
       sign_in user
     end
   end
-
 
   def sign_in(user)
     visit root_path
